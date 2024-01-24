41ef7832f103a565c4a89cf34a9f988b93fe17c5
Faizan-zia340 <154322813+Faizan-zia340@users.noreply.github.com>
2024-01-24 13:46:14 +0500

    task

---
+++ task.py
@@ -0,0 +1,38 @@
+
+a=(".append")
+print(a)
+fruits=['mango','banana','apple']
+fruits.append('grapes')
+print(fruits)
+
+
+d=("del specific element")
+print(d)
+fruits=['mango','banana','apple','grapes']
+print()
+del fruits[3]
+print(fruits)
+
+c=("concatenation")
+print(c)
+list1=['1','2','3']
+list2=['4','5','6']
+newlist=list1+list2
+print(newlist)
+c=("numbers*2")
+print(c)
+numbers = [3, 5, 7, 9]
+doubled_numbers = [num * 2 for num in numbers]
+print(doubled_numbers)
+
+t=("tuple")
+print(t)
+week=('monday','tuesday','wednesday','thursday','friday','saturday','sunday')
+print(week)
+
+a=(" access third day of tupple")
+print(a)
+print (week[3])
+p=("people info")
+people_info=[('babar',25),('rizwan', 27),('naseem',21),('hasnain',22)]
+print(people_info)                                       
